2e4a88f282fb8ff06d3af70946a1a99aacf31419
jkyla <52054379+jkyla@users.noreply.github.com>
2019-09-23 20:07:10 -0500

    Add files via upload

---
+++ Project3.cpp
@@ -0,0 +1,30 @@
+//Kyla Johnson
+//COP2000.0M1
+//This program calculates the number of points assigned to book purchases per month
+
+#include <iostream>
+#include <iomanip>
+using namespace std;
+
+int main()
+{
+	//Variables
+	int books; //the number of books the user has purchased
+	int points; //the number of points assigned to each book purchase
+	int awards; //the calculated total number of points earned 
+
+	cout << "Monthly Book Club Awards" << endl;
+
+	//Ask the user to input the number of books purchased
+	cout << "Enter the number of books purchased this month: ";
+	cin >> books;
+
+
+
+
+
+
+
+
+	return 0;
+}
