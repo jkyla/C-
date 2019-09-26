97b94d563e89af5f8938c50fb7f502bd467d1de0
jkyla <52054379+jkyla@users.noreply.github.com>
2019-09-26 15:40:44 -0500

    Add files via upload

--- Project3.cpp
+++ Project3.cpp
@@ -9,21 +9,39 @@ using namespace std;
 int main()
 {
 	//Variables
-	int books; //the number of books the user has purchased
-	int points; //the number of points assigned to each book purchase
-	int awards; //the calculated total number of points earned 
+	int sumBooks; //the number of books the user has purchased
+	int sumAwards; //the number of points assigned to each book purchase
+	 
 
-	cout << "Monthly Book Club Awards" << endl;
+	cout << "Monthly Book Club Awards \n\n" << endl;
 
 	//Ask the user to input the number of books purchased
 	cout << "Enter the number of books purchased this month: ";
-	cin >> books;
+	cin >> sumBooks;
+
+	//Validate user input
 
 
+	//Calculate the number of awards per month
 
+	if (sumBooks < 1)
+		sumAwards = 0;
 
+	else if (sumBooks == 1)
+		sumAwards = 5;
 
+	else if (sumBooks == 2)
+		sumAwards = 15;
 
+	else if (sumBooks == 3)
+		sumAwards = 30;
+
+	else if (sumBooks >= 4)
+		sumAwards = 50;
+
+	//Display the number of awards per month
+
+	cout << " \nCongrats! You have earned " << sumAwards << " points this month. " << endl;
 
 
 	return 0;
