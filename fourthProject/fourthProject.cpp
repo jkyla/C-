744d2bd88492e45ab78560a33f9dc6918c398910
jkyla <52054379+jkyla@users.noreply.github.com>
2019-10-04 13:53:24 -0500

    Add files via upload

---
+++ fourthProject/fourthProject.cpp
@@ -0,0 +1,34 @@
+//Kyla Johnson
+//COP2000.0M1
+//This program reads an input file to display sales data per store and displays it in the form of a bar graph
+
+#include <iostream>
+#include <fstream> // used to read file
+
+using namespace std;
+
+int main()
+{
+
+
+
+
+
+
+
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	return 0;
+}
